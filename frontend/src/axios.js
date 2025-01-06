b35d2b8ed7663b1b518ac91af29c54d8eb368a27
fortyguide <stefanopanico94@gmail.com>
2025-01-06 01:00:07 +0100

    reset frontend

--- frontend/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://localhost:3000/api',
-  withCredentials: true, // Permette l'invio di cookie con le richieste
-});
-
-export default instance;
